d7019d72317f15a9b36fcbdff99fe197a9e1e86d
ahmad <asalameh02gmail.comm>
2025-02-10 16:46:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PostApp());
+}
+
+class PostApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
